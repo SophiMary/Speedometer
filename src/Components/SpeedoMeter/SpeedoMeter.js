2be013e11ead5f9cd4c36a1824d3451076b4efad
Sophie Mary <sophiemary@Phoenix.local>
2020-11-27 20:18:02 +0530

    init

---
+++ src/Components/SpeedoMeter/SpeedoMeter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./SpeedoMeter.css"
+
+export default function Speedometer() {
+    return (
+        <div className="gauge">
+            <div className="gaugeBody">
+                <hi>Hi</hi>
+            </div>
+        </div>
+    )
+}
