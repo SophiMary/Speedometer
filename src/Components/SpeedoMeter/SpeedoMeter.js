d71b4e8c22a2d3671e6b3e181789048151a53dac
Sophie Mary <sophiemary@Phoenix.local>
2020-11-28 01:52:45 +0530

    add input field with loop

--- src/Components/SpeedoMeter/SpeedoMeter.js
+++ src/Components/SpeedoMeter/SpeedoMeter.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import "./SpeedoMeter.css"
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
+import "./SpeedoMeter.css";
 
 export default function Speedometer() {
+    let [ speedValue, setSpeedValue ] = useState(0);
+    useEffect(() => {
+        if (speedValue < 225) {
+            const interval = setInterval(() => {
+                setSpeedValue((speedValue) => speedValue + 5);
+            }, 1000);
+            return () => {
+                  clearInterval(interval);
+                };
+        } else 
+            setSpeedValue(0)
+    }, [speedValue]);
+    
     return (
         <div className="gauge">
-            <div className="gaugeBody">
-                <hi>Hi</hi>
-            </div>
+            <section className="gaugeBody">
+                <div>
+                    <form id="drawTemp">
+                        <input type="number" id="speed" name="speed" value={speedValue} readOnly/>
+                    </form>
+                </div>
+            </section>
         </div>
-    )
+    );
 }
